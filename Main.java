8c0da9d4f81a10468ed8654579e5c03143953054
Ketan Ramteke <Ketantoo@gmail.com>
2018-04-11 21:37:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public */
+/* Name of the class has to be "Main" only if the class ispublic */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
